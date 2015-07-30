fbde11e7a1796bd1e252ce0d787275654e31cd70
martinmaricak <martin@maricak.sk>
2015-07-30 16:21:04 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $(function() {
+    $('a[href*=#]:not([href=#])').click(function(e) {
+        e.preventDefault();
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 600);
+          return false;
+        }
+      }
+    });
+  });
+
+});
